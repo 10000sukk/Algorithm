b1d5d0707b22e3f7bd7071ff0b87cc6da49b003f
10000sukk <bcrhs4460@gmail.com>
2020-08-19 00:00:01 +0900

    remove

--- 이취코/3-3.py
+++
@@ -1,9 +0,0 @@
-#숫자 카드 게임
-n,m= map(int, input().split())
-card = []
-for i in range(n):
-    card.append(list(map(int, input().split())))
-result = []
-for i in card:
-    result.append(min(i))
-print(max(result))
